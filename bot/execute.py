f6759b711bf5cca651937d4b42647508d8ab6b10
mike-kob <37714867+mike-kob@users.noreply.github.com>
2019-09-17 13:51:34 +0300

    Add execute

---
+++ bot/execute.py
@@ -0,0 +1,16 @@
+import sys
+from Bot import Bot
+
+bot = Bot()
+
+if __name__ == '__main__':
+    command = sys.argv[1]
+    if command == 'move':
+        if len(sys.argv) < 4:
+            raise ValueError('Incorrect usage. \n'
+                             'python execute.py move LEFT_WHEEL RIGHT WHEEL DURATION')
+        left = int(sys.argv[2])
+        right = int(sys.argv[3])
+        duration = int(sys.argv[4])
+
+        bot.move(left, right, duration)
