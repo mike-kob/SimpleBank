ce8401e434fdeec88a275c3ae1c279ecc78d8201
Vadym <kochmar.vadym@gmail.com>
2019-10-13 12:59:09 +0300

    Fixed project structure and data types

---
+++ server/BankServer/BankServer/Models/CreditCard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank_server.Models
+{
+    public class CreditCard : ICard
+    {
+        [Key]
+        public long CardNum { get; set; }
+        [Required]
+        [MaxLength(4)]
+        public string Pin { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public decimal OwnMoney { get; set; }
+        [Required]
+        public decimal Limit { get; set; }
+        [Required]
+        public decimal Balance { get; }
+        public double PercentIfDelay { get; } = 1.0;
+        public bool IsInLimit { get; set; }
+        public DateTime? LimitWithdrawn { get; set; }
+        public DateTime? EndLimit { get; set; }
+        public bool? IsLimitPaid { get; set; }
+
+        [ForeignKey("Id")]
+        public User CardUser { get; set; }
+        public int Id { get; set; }
+
+        //
+        public DateTime? EndLimitDate()
+        {
+            //TO DO
+            //you can replenish the account without any percent until calculated date - end of the next month
+            return null;
+        }
+    }
+}
---
+++ server/BankServer/BankServer/Models/DepositCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank_server.Models
+{
+    public class DepositCard : ICard
+    {
+        [Key]
+        public long CardNum { get; set; }
+        [Required]
+        [MaxLength(4)]
+        public string Pin { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        // initial sum
+        [Required]
+        public decimal Balance { get; set; }
+        [Required]
+        public decimal Rate { get; set; }
+        [Required]
+        public decimal TotalBalance => UpdateBalance() ? Balance + Balance * Rate : Balance;
+        [Required]
+        public DateTime StartDeposit { get; set; }
+        [Required]
+        public DateTime EndDeposit { get => new DateTime(StartDeposit.Year + 1, StartDeposit.Month, StartDeposit.Day, StartDeposit.Hour, StartDeposit.Minute, StartDeposit.Second); }
+        //property to check  if user withdraw money before the end of the deposit
+        [Required]
+        public bool Commission { get; set; }
+        [Required]
+        public double PercentIfWithdraw { get; } = 1.0;
+        [ForeignKey("Id")]
+        public User CardUser { get; set; }
+        public int Id { get; set; }
+
+
+        private bool UpdateBalance()
+        {
+            //code to check if we need to add bonus
+            return false;
+        }
+    }
+}
+
