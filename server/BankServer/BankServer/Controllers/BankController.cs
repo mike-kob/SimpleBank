4fd6ae81a51bca4c75ddcc6ce9851978b0397d16
Vadym <kochmar.vadym@gmail.com>
2019-10-14 18:31:05 +0300

    added gitignore

--- server/BankServer/BankServer/Controllers/BankController.cs
+++ server/BankServer/BankServer/Controllers/BankController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BankServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class BankController : ControllerBase
     {
@@ -18,6 +18,42 @@ namespace BankServer.Controllers
         public BankController(BankServerContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("balance/{id}")]
+        public async Task<ActionResult> GetBalanceDepositCard(long id)
+        {
+
+            if (await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
+            {
+                var card = await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id);
+                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
+            }
+            else if (await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
+            {
+                var card = await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id);
+                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
+            }
+            else if (await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
+            {
+                var card = await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id);
+                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
+            }
+            return new OkObjectResult(new BalanceCard { Ok = false, CardNum = id, Balance = 0 });
+        }
+
+
+        public class CardExists
+        {
+            public bool Ok { get; set; }
+            public long CardNum { get; set; }
+            public bool IsValid { get; set; }
+        }
+        public class BalanceCard
+        {
+            public bool Ok { get; set; }
+            public long CardNum { get; set; }
+            public decimal Balance { get; set; }
         }
     }
 }
