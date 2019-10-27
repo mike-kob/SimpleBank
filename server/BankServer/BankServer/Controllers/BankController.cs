4f3850f8779dc64b0eb190d894f4118936f9c17b
Vadym Kochmar <47254934+vvadymk@users.noreply.github.com>
2019-10-27 15:08:06 +0200

    Revert "Get requests"

--- server/BankServer/BankServer/Controllers/BankController.cs
+++ server/BankServer/BankServer/Controllers/BankController.cs
@@ -4,13 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BankServer.Controllers
 {
-    [Route("api")]
+    [Route("api/[controller]")]
     [ApiController]
     public class BankController : ControllerBase
     {
@@ -19,159 +18,6 @@ namespace BankServer.Controllers
         public BankController(BankServerContext context)
         {
             _context = context;
-        }
-
-        [HttpGet("cardExists/{id}")]
-        public async Task<ActionResult> GetCardExists(long id)
-        {
-
-            if (await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new CardExists { Ok = true, CardNum = id, IsValid = true });
-            }
-            else if (await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new CardExists { Ok = true, CardNum = id, IsValid = true });
-            }
-            else if (await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new CardExists { Ok = true, CardNum = id, IsValid = true });
-            }
-            return new OkObjectResult(new BalanceCard { Ok = false, CardNum = id, Balance = 0 });
-        }
-
-        [HttpGet("balance/{id}")]
-        public async Task<ActionResult> GetBalanceCard(long id)
-        {
-
-            if (await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.CheckingCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
-            }
-            else if (await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.DepositCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
-            }
-            else if (await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id) != null)
-            {
-                var card = await _context.CreditCard.SingleOrDefaultAsync(m => m.CardNum == id);
-                return new OkObjectResult(new BalanceCard { Ok = true, CardNum = id, Balance = card.Balance });
-            }
-            return new OkObjectResult(new BalanceCard { Ok = false, CardNum = id, Balance = 0 });
-        }
-
-        [HttpPut("changePin")]
-        public async Task<ActionResult<CheckingCard>> PutChangePinChecking()
-        {
-            var body = "";
-            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
-            {
-                body = await stream.ReadToEndAsync();
-            }
-            dynamic myObject = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(body);
-            long cardNum = Convert.ToInt64(myObject.num);
-            string oldPin = Convert.ToString(myObject.old);
-            string newPin = Convert.ToString(myObject.newp);
-
-            try
-            {
-                if (_context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin==oldPin)
-                {
-                    _context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin=newPin;
-                    await _context.SaveChangesAsync();
-                }
-                else
-                {
-                    return Forbid();
-                }
-            }
-            catch (ArgumentNullException)
-            {
-                return Unauthorized();
-            }
-            return Ok("allowed");
-        }
-
-        [HttpPut("changePin")]
-        public async Task<ActionResult<DepositCard>> PutChangePinDeposit()
-        {
-            var body = "";
-            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
-            {
-                body = await stream.ReadToEndAsync();
-            }
-            dynamic myObject = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(body);
-            long cardNum = Convert.ToInt64(myObject.num);
-            string oldPin = Convert.ToString(myObject.old);
-            string newPin = Convert.ToString(myObject.newp);
-
-            try
-            {
-                if (_context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin == oldPin)
-                {
-                    _context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin = newPin;
-                    await _context.SaveChangesAsync();
-                }
-                else
-                {
-                    return Forbid();
-                }
-            }
-            catch (ArgumentNullException)
-            {
-                return Unauthorized();
-            }
-            return Ok("allowed");
-        }
-
-        [HttpPut("changePin")]
-        public async Task<ActionResult<CreditCard>> PutChangePinCredit()
-        {
-            var body = "";
-            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
-            {
-                body = await stream.ReadToEndAsync();
-            }
-            dynamic myObject = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(body);
-            long cardNum = Convert.ToInt64(myObject.num);
-            string oldPin = Convert.ToString(myObject.old);
-            string newPin = Convert.ToString(myObject.newp);
-
-            try
-            {
-                if (_context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin == oldPin)
-                {
-                    _context.CheckingCard.FirstOrDefault(c => c.CardNum == cardNum).Pin = newPin;
-                    await _context.SaveChangesAsync();
-                }
-                else
-                {
-                    return Forbid();
-                }
-            }
-            catch (ArgumentNullException)
-            {
-                return Unauthorized();
-            }
-            return Ok("allowed");
-        }
-
-        public class CardExists
-        {
-            public bool Ok { get; set; }
-            public long CardNum { get; set; }
-            public bool IsValid { get; set; }
-        }
-        public class BalanceCard
-        {
-            public bool Ok { get; set; }
-            public long CardNum { get; set; }
-            public decimal Balance { get; set; }
         }
     }
 }
