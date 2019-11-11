9100328189a49a595ca14a2d793706bb2b1c99a1
Тютюн Марина Миколаївна <m.tyutyun@ukma.edu.ua>
2019-11-11 12:16:55 +0200

    fix bugs

--- server/BankServer/BankServer/Migrations/20191110212930_master1.Designer.cs
+++ server/BankServer/BankServer/Migrations/20191110212930_master1.Designer.cs
@@ -4,14 +4,16 @@ using BankServer.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankServer.Migrations
 {
     [DbContext(typeof(BankServerContext))]
-    partial class BankServerContextModelSnapshot : ModelSnapshot
+    [Migration("20191110212930_master1")]
+    partial class master1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
