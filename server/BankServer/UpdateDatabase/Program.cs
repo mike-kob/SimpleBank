d2fcd4c2d41c4a0a12dae46fc6403aed37dc73f6
Тютюн Марина Миколаївна <m.tyutyun@ukma.edu.ua>
2019-11-23 22:40:17 +0200

    Fix updating of credit card balance

--- server/BankServer/UpdateDatabase/Program.cs
+++ server/BankServer/UpdateDatabase/Program.cs
@@ -18,21 +18,23 @@ namespace UpdateDatabase
         static void Main(string[] args)
         {
             _optionsBuilder = new DbContextOptionsBuilder<BankServerContext>();
-            _optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BankServer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BankServer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=true");
             //_optionsBuilder.UseSqlServer("Server=MICHAEL-PC\\SQLEXPRESS;Database=bankDB_6;Trusted_Connection=True;MultipleActiveResultSets=true");
             using (_context = new BankServerContext(_optionsBuilder.Options))
             {
                 var creditCards = _context.CreditCard;
                 var depositCards = _context.DepositCard;
-                HandleTimer(depositCards, creditCards);
-                var timer = new Timer(1000 * 86400);
-                timer.Elapsed += async (sender, e) => await HandleTimer(depositCards, creditCards);
+                var transactions = _context.Transaction;
+                HandleTimer(depositCards, creditCards, transactions);
+                var timer = new Timer(1000 * 5);
+                //var timer = new Timer(1000 * 86400);
+                timer.Elapsed += async (sender, e) => await HandleTimer(depositCards, creditCards, transactions);
                 timer.Start();
                 Console.ReadKey();
             }
         }
 
-        private static Task HandleTimer(IQueryable<DepositCard> depositCards, IQueryable<CreditCard> creditCards)
+        private static Task HandleTimer(IQueryable<DepositCard> depositCards, IQueryable<CreditCard> creditCards, IQueryable<Transaction> transactions)
         {
             if (depositCards.LongCount() > 0)
             {
@@ -44,7 +46,7 @@ namespace UpdateDatabase
             {
                 Thread.Sleep(1000);
                 Console.WriteLine($"credit cards were updated at {DateTime.Now.ToString(CultureInfo.CurrentCulture)}");
-                UpdateCredit(creditCards);
+                UpdateCredit(creditCards, transactions);
                 _context.SaveChangesAsync();
             }
             return Task.CompletedTask;
@@ -60,17 +62,32 @@ namespace UpdateDatabase
                 card.TotalBalance = card.Balance + card.Balance * card.Rate;
             }
         }
-        private static void UpdateCredit(IEnumerable<CreditCard> cards)
+        private static void UpdateCredit(IEnumerable<CreditCard> cards, IEnumerable<Transaction> transactions)
         {
             foreach (var card in cards)
             {
                 if (!card.IsInLimit) continue;
                 if (card.Balance < 0) continue;
                 if (!(card.EndLimit < DateTime.Now)) continue;
+                var days = (DateTime.Now - card.EndLimit).Value.Days;
+                var lastTransaction = transactions.FirstOrDefault(x => x.TypeOfTxn == 2 && x.CardSenderNum == card.CardNum);
+                if (lastTransaction != null)
+                {
+                    days = (DateTime.Now - lastTransaction.DatetimeOfTxn).Days;
+                }
                 if (card.MinSum != null)
                 {
-                    var days = (DateTime.Now - card.EndLimit).Value.Days;
-                    card.OwnMoney -= card.MinSum.Value * card.PercentIfDelay * days;
+                    var withdrawalSum = card.MinSum.Value * card.PercentIfDelay * days;
+                    var transaction = new Transaction
+                    {
+                        TypeOfTxn = 2,
+                        Amount = withdrawalSum,
+                        DatetimeOfTxn = DateTime.Now,
+                        CardSenderNum = card.CardNum
+                    };
+                    _context.Transaction.Add(transaction);
+                    _context.SaveChangesAsync();
+                    card.OwnMoney -= withdrawalSum;
                 }
             }
         }
